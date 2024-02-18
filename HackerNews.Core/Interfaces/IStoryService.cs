ab4efd3b1245d6e773cbe3e2658f82af34c3abb2
Oleksii Remizov <remizovtlalex@gmail.com>
2024-02-18 16:32:44 +0200

    Story service has been adjusted.

--- HackerNews.Core/Interfaces/IStoryService.cs
+++ HackerNews.Core/Interfaces/IStoryService.cs
@@ -4,6 +4,8 @@ namespace HackerNews.Core.Services
 {
     public interface IStoryService
     {
-        Task<IEnumerable<Story>> GetBestStories(int count);
+        Task<IEnumerable<Story>> GetStoryDetailsAsync(IEnumerable<int> storyIds);
+
+        Task<IEnumerable<int>> GetBestStoryIdsWithRetryAsync(int maxAttempts);
     }
 }
--- HackerNews.Core/Services/StoryService.cs
+++ HackerNews.Core/Services/StoryService.cs
@@ -11,7 +11,7 @@ namespace HackerNews.Core.Services
         private readonly HttpClient _httpClient;
         private readonly IMapper _mapper;
         private readonly IConfiguration _configuration;
-        private static readonly JsonSerializerOptions Options = new JsonSerializerOptions
+        private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
         {
             PropertyNameCaseInsensitive = true
         };
@@ -23,22 +23,14 @@ namespace HackerNews.Core.Services
             _configuration = configuration;
         }
 
-        public async Task<IEnumerable<Story>> GetBestStories(int count)
+        public async Task<IEnumerable<Story>> GetStoryDetailsAsync(IEnumerable<int> storyIds)
         {
-            var bestStoryIds = await GetBestStoryIdsWithRetry();
-            var stories = await GetStoryDetails(bestStoryIds.Take(count));
-
-            return stories.OrderByDescending(s => s.Score);
-        }
-
-        private async Task<IEnumerable<Story>> GetStoryDetails(IEnumerable<int> storyIds)
-        {
-            var tasks = storyIds.Select(id => GetStoryDetailWithRetry(id));
+            var tasks = storyIds.Select(id => GetStoryDetailWithRetryAsync(id));
             var stories = await Task.WhenAll(tasks);
             return stories.Where(s => s != null);
         }
 
-        private async Task<IEnumerable<int>> GetBestStoryIdsWithRetry(int maxAttempts = 3)
+        public async Task<IEnumerable<int>> GetBestStoryIdsWithRetryAsync(int maxAttempts = 3)
         {
             for (int attempt = 1; attempt <= maxAttempts; attempt++)
             {
@@ -48,29 +40,25 @@ namespace HackerNews.Core.Services
                     response.EnsureSuccessStatusCode();
 
                     var contentStream = await response.Content.ReadAsStreamAsync();
-                    var bestStoryIds = await JsonSerializer.DeserializeAsync<IEnumerable<int>>(contentStream, Options);
+                    var bestStoryIds = await JsonSerializer.DeserializeAsync<IEnumerable<int>>(contentStream, _options);
 
                     return bestStoryIds;
                 }
-                catch (HttpRequestException ex)
+                catch (HttpRequestException)
                 {
-                    // Если это последняя попытка, пробросить исключение дальше
                     if (attempt == maxAttempts)
                     {
                         throw;
                     }
 
-                    // Если это не последняя попытка, вывести сообщение об ошибке и повторить попытку через некоторое время
-                    Console.WriteLine($"Failed to retrieve best story IDs on attempt {attempt}. Retrying in 1 second. Error: {ex.Message}");
                     await Task.Delay(TimeSpan.FromSeconds(1));
                 }
             }
 
-            // Недостижимая ветка кода, возвращаем null, если все попытки были неудачными
             return null;
         }
 
-        private async Task<Story> GetStoryDetailWithRetry(int storyId, int maxAttempts = 3)
+        private async Task<Story> GetStoryDetailWithRetryAsync(int storyId, int maxAttempts = 3)
         {
             for (int attempt = 1; attempt <= maxAttempts; attempt++)
             {
@@ -80,25 +68,21 @@ namespace HackerNews.Core.Services
                     response.EnsureSuccessStatusCode();
 
                     using var contentStream = await response.Content.ReadAsStreamAsync();
-                    var storyDto = await JsonSerializer.DeserializeAsync<StoryDto>(contentStream, Options);
+                    var storyDto = await JsonSerializer.DeserializeAsync<StoryDto>(contentStream, _options);
 
                     return _mapper.Map<Story>(storyDto);
                 }
-                catch (HttpRequestException ex)
+                catch (HttpRequestException)
                 {
-                    // Если это последняя попытка, пробросить исключение дальше
                     if (attempt == maxAttempts)
                     {
                         throw;
                     }
 
-                    // Если это не последняя попытка, вывести сообщение об ошибке и повторить попытку через некоторое время
-                    Console.WriteLine($"Failed to retrieve story with ID {storyId} on attempt {attempt}. Retrying in 1 second. Error: {ex.Message}");
                     await Task.Delay(TimeSpan.FromSeconds(1));
                 }
             }
 
-            // Недостижимая ветка кода, возвращаем null, если все попытки были неудачными
             return null;
         }
     }
